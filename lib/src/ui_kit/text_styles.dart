3633b13a72ea7eff85b0729b189d19c2ed8512d2
ericFlutterast <erikGitFlutter@gmail.com>
2024-06-03 19:54:25 +0300

    common text styles been moved to UiKitTextStyles

---
+++ lib/src/ui_kit/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UiKitTextStyles {
+  static TextStyle buttonStyle = TextStyle(
+    fontSize: 15,
+    color: Colors.black.withOpacity(0.8),
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle titleStyle = TextStyle(
+    fontSize: 26,
+    color: Colors.black.withOpacity(0.8),
+    fontWeight: FontWeight.w700,
+  );
+}
