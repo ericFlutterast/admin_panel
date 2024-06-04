f3f72fdd5fa82a4cddda76eac3d536cac5f076fa
ericFlutterast <erikGitFlutter@gmail.com>
2024-06-05 00:04:28 +0300

    subject repository

---
+++ lib/src/features/subjects/data/repositories/subject_repository.dart
@@ -0,0 +1,77 @@
+import 'package:admin_panel_for_library/src/common/network_client/network_client.dart';
+import 'package:admin_panel_for_library/src/features/subjects/data/repositories_interface/subject_repository_interface.dart';
+
+final class SubjectRepository implements ISubjectRepository {
+  SubjectRepository({
+    required NetworkClient networkClient,
+  }) : _networkClient = networkClient;
+
+  final NetworkClient _networkClient;
+
+  @override
+  Future<void> createSubject({required String title}) async {
+    await _networkClient.request(
+      type: Post(
+        path: '/subjects',
+        data: {'title': title},
+      ),
+    );
+  }
+
+  @override
+  Future<void> deleteSubject({required int id}) async {
+    await _networkClient.request(
+      type: Delete(path: '/subjects/$id'),
+    );
+  }
+
+  @override
+  Future<Subject> getSubject({int? id}) async {
+    await _networkClient.request(
+      type: Get(path: '/subjects/${id ?? ''}'),
+    );
+
+    //TODO:
+    return 1;
+  }
+
+  @override
+  Future<void> linkBook({required int bookId}) async {
+    await _networkClient.request(type: Put(path: '/subjects/$bookId/link/book'));
+  }
+
+  @override
+  Future<void> linkCourse({required int courseId}) async {
+    await _networkClient.request(type: Put(path: '/subjects/$courseId/link/course'));
+  }
+
+  @override
+  Future<void> linkFaculty({required int facultyId}) async {
+    await _networkClient.request(type: Put(path: '/subjects/$facultyId/link/faculty'));
+  }
+
+  @override
+  Future<void> linkField({required int fieldId}) async {
+    await _networkClient.request(type: Put(path: '/subjects/$fieldId/link/field'));
+  }
+
+  @override
+  Future<void> unlinkBook({required int bookId}) async {
+    await _networkClient.request(type: Delete(path: '/subjects/$bookId/link/book'));
+  }
+
+  @override
+  Future<void> unlinkCourse({required int courseId}) async {
+    await _networkClient.request(type: Delete(path: '/subjects/$courseId/link/course'));
+  }
+
+  @override
+  Future<void> unlinkFaculty({required int facultyId}) async {
+    await _networkClient.request(type: Delete(path: '/subjects/$facultyId/link/faculty'));
+  }
+
+  @override
+  Future<void> unlinkField({required int fieldId}) async {
+    await _networkClient.request(type: Delete(path: '/subjects/$fieldId/link/field'));
+  }
+}
