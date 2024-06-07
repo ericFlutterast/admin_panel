6c7d74238daddb520832ea17cdc71af257d5cb5f
ericFlutterast <erikGitFlutter@gmail.com>
2024-06-07 19:38:21 +0300

    custom dropped widget helper_selector.dart

--- lib/src/features/subject_management/select_pdf_to_attach_to_subject/ui/widgets/select_pdf_to_attach_to_subject.dart
+++ lib/src/features/subject_management/select_pdf_to_attach_to_subject/ui/widgets/select_pdf_to_attach_to_subject.dart
@@ -1,3 +1,4 @@
+import 'package:admin_panel_for_library/src/features/subject_management/select_pdf_to_attach_to_subject/ui/widgets/helper_selector.dart';
 import 'package:admin_panel_for_library/src/ui_kit/text_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class SelectPdfToAttachToSubjectModal extends StatelessWidget {
           width: width,
           decoration: const BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderRadius: const BorderRadius.all(Radius.circular(30)),
           ),
           child: CustomScrollView(
             slivers: [
@@ -35,8 +36,12 @@ class SelectPdfToAttachToSubjectModal extends StatelessWidget {
                     SizedBox(
                       height: 55,
                       width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
+                      child: HelperSelector(
+                        droppedChildConstraints: BoxConstraints(
+                          maxHeight: height * 1.5,
+                          maxWidth: width * 1.5,
+                        ),
+                        droppedChild: const _SelectFromLibrary(),
                         child: Text(
                           'Выбрать из библиотеки',
                           style: UiKitTextStyles.buttonStyle,
@@ -47,8 +52,12 @@ class SelectPdfToAttachToSubjectModal extends StatelessWidget {
                     SizedBox(
                       height: 55,
                       width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
+                      child: HelperSelector(
+                        droppedChild: Container(
+                          height: 200,
+                          width: width,
+                          color: Colors.green,
+                        ),
                         child: Text(
                           'Загрузить и прикрепить',
                           style: UiKitTextStyles.buttonStyle,
@@ -56,6 +65,70 @@ class SelectPdfToAttachToSubjectModal extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SelectFromLibrary extends StatelessWidget {
+  const _SelectFromLibrary();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      borderRadius: const BorderRadius.all(Radius.circular(30)),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+          border: Border.all(
+            color: Colors.black.withOpacity(0.7),
+            width: 2,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child: SizedBox(height: 16),
+              ),
+              SliverToBoxAdapter(
+                child: Center(
+                  child: Text(
+                    'Библиотека',
+                    style: UiKitTextStyles.titleStyle,
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Divider(color: Colors.black.withOpacity(0.1)),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: 10,
+                  (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 5),
+                      height: 50,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                      child: const Center(
+                        child: Text('Демидович.pdf'),
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
